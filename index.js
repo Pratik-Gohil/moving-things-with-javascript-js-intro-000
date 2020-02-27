eef23b78efbea0c36882ba040f41034de743ef86
Pratik <pr.tikgohil@gmail.com>
2020-02-27 09:26:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = '#FF69B4'
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+    console.log(dodger.style.left)
+
+  } else if (e.which === 39) {
+    moveDodgerRight()
+    console.log(dodger.style.right)
+  }
+})
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`
+  }
+}
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  console.log(rightNumbers)
+  var right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.left = `${right - 10}px`
+  }
+}
