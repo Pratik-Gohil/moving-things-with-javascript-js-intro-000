b51d3fb38027ba6c9de294c14e1b1b473f0042e4
Pratik <pr.tikgohil@gmail.com>
2020-02-27 09:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
 
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`
+  }
 }
